fcaff28c01ac78e9b67671c74c7afb062847b112
VitoCarlenG <xlhvitocarlen@gmail.com>
2021-12-03 22:54:10 +0700

    Unit Testing - Instrumented

---
+++ app/src/androidTest/java/com/example/uts_pbp/AddEditActivityTest.java
@@ -0,0 +1,277 @@
+package com.example.uts_pbp;
+
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import com.example.uts_pbp.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AddEditActivityTest {
+
+    @Rule
+    public ActivityTestRule<AddEditActivity> mActivityTestRule = new ActivityTestRule<>(AddEditActivity.class);
+
+    @Test
+    public void addEditActivityTest() {
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.btn_save), withText("Simpan"),
+                        childAtPosition(
+                                allOf(withId(R.id.ll_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        onView(isRoot()).perform(waitFor(4000));
+
+        ViewInteraction textInputEditText = onView(
+                allOf(withId(R.id.et_nama),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_nama),
+                                        0),
+                                1)));
+        textInputEditText.perform(scrollTo(), replaceText("1"), closeSoftKeyboard());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.btn_save), withText("Simpan"),
+                        childAtPosition(
+                                allOf(withId(R.id.ll_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        onView(isRoot()).perform(waitFor(4000));
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(withId(R.id.et_nama), withText("1"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_nama),
+                                        0),
+                                1)));
+        textInputEditText2.perform(scrollTo(), replaceText("$"));
+
+        ViewInteraction textInputEditText3 = onView(
+                allOf(withId(R.id.et_nama), withText("$"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_nama),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textInputEditText3.perform(closeSoftKeyboard());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.btn_save), withText("Simpan"),
+                        childAtPosition(
+                                allOf(withId(R.id.ll_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        onView(isRoot()).perform(waitFor(4000));
+
+        ViewInteraction textInputEditText4 = onView(
+                allOf(withId(R.id.et_nama), withText("$"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_nama),
+                                        0),
+                                1)));
+        textInputEditText4.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText5 = onView(
+                allOf(withId(R.id.et_nama), withText("$"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_nama),
+                                        0),
+                                1)));
+        textInputEditText5.perform(scrollTo(), replaceText("AB"));
+
+        ViewInteraction textInputEditText6 = onView(
+                allOf(withId(R.id.et_nama), withText("AB"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_nama),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textInputEditText6.perform(closeSoftKeyboard());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.btn_save), withText("Simpan"),
+                        childAtPosition(
+                                allOf(withId(R.id.ll_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        onView(isRoot()).perform(waitFor(4000));
+
+        ViewInteraction textInputEditText7 = onView(
+                allOf(withId(R.id.et_nama), withText("AB"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_nama),
+                                        0),
+                                1)));
+        textInputEditText7.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText8 = onView(
+                allOf(withId(R.id.et_nama), withText("AB"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_nama),
+                                        0),
+                                1)));
+        textInputEditText8.perform(scrollTo(), replaceText("A"));
+
+        ViewInteraction textInputEditText9 = onView(
+                allOf(withId(R.id.et_nama), withText("A"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_nama),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textInputEditText9.perform(closeSoftKeyboard());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.btn_save), withText("Simpan"),
+                        childAtPosition(
+                                allOf(withId(R.id.ll_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton5.perform(click());
+
+        onView(isRoot()).perform(waitFor(4000));
+
+        ViewInteraction textInputEditText10 = onView(
+                allOf(withId(R.id.et_harga),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_harga),
+                                        0),
+                                1)));
+        textInputEditText10.perform(scrollTo(), replaceText("Apel"), closeSoftKeyboard());
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(R.id.btn_save), withText("Simpan"),
+                        childAtPosition(
+                                allOf(withId(R.id.ll_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton6.perform(click());
+
+        onView(isRoot()).perform(waitFor(4000));
+
+        ViewInteraction textInputEditText12 = onView(
+                allOf(withId(R.id.et_deskripsi),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.layout_deskripsi),
+                                        0),
+                                1)));
+        textInputEditText12.perform(scrollTo(), replaceText("https://cdn-brilio-net.akamaized.net/community/2018/09/12/13793/ayo-nikmati-apel-rebus-yang-ternyata-sangat-baik-untuk-kesehatan.jpg"), closeSoftKeyboard());
+
+        ViewInteraction materialButton7 = onView(
+                allOf(withId(R.id.btn_save), withText("Simpan"),
+                        childAtPosition(
+                                allOf(withId(R.id.ll_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton7.perform(click());
+
+        onView(isRoot()).perform(waitFor(4000));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    public static ViewAction waitFor(long delay) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+            @Override
+            public String getDescription() {
+                return "wait for " + delay + "milliseconds";
+            }
+            @Override
+            public void perform(UiController uiController, View view) {
+                uiController.loopMainThreadForAtLeast(delay);
+            }
+        };
+    }
+}
