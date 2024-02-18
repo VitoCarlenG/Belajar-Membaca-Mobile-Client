17b40255352c7c1421d00ae8c57541be8df0961d
Vito Carlen Giovanni <vitocarlengiovanni@gmail.com>
2024-02-18 15:44:29 +0700

    Update NoteApi.java

--- app/src/main/java/com/example/uts_pbp/api/NoteApi.java
+++ app/src/main/java/com/example/uts_pbp/api/NoteApi.java
@@ -1,11 +1,11 @@
 package com.example.uts_pbp.api;
 
 public class NoteApi {
-    public static final String BASE_URL = "https://thawing-sea-68275.herokuapp.com/api/";
+    public static final String BASE_URL = "https://thawing-sea-68275.herokuapp.com/api";
 
-    public static final String GET_ALL_URL = BASE_URL + "note";
-    public static final String GET_BY_ID_URL = BASE_URL + "note/";
-    public static final String ADD_URL = BASE_URL + "note";
-    public static final String UPDATE_URL = BASE_URL + "note/";
-    public static final String DELETE_URL = BASE_URL + "note/";
+    public static final String GET_ALL_URL = BASE_URL + "/note";
+    public static final String GET_BY_ID_URL = BASE_URL + "/note/";
+    public static final String ADD_URL = BASE_URL + "/note";
+    public static final String UPDATE_URL = BASE_URL + "/note/";
+    public static final String DELETE_URL = BASE_URL + "/note/";
 }
